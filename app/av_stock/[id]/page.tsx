6a69068581ce1503abe21207101f8b57067bd82c
Onyshchenko Serhii <123294835+19taurus79@users.noreply.github.com>
2025-07-25 15:41:33 +0300

    add av stock in other warehouses

---
+++ app/av_stock/[id]/page.tsx
@@ -0,0 +1,27 @@
+import BackBtn from "@/components/BackBtn/BackBtn";
+import css from "./AvRemainsList.module.css";
+import { getAvRemainsById } from "@/lib/api";
+type Props = {
+  params: Promise<{ id: string }>;
+};
+export default async function AvStockPage({ params }: Props) {
+  const id = await params;
+  const remains = await getAvRemainsById({ productId: id.id });
+  console.log("av stock remains", remains);
+  return (
+    <>
+      <div className={css.container}>
+        <h2 className={css.heading}>{remains[0].nomenclature} вільно по РУ</h2>
+        <ul className={css.table}>
+          {remains.map((item) => (
+            <li key={item.id} className={css.row}>
+              <span className={css.cell_division}>{item.division}</span>
+              <span className={css.cell_available}>{item.available}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <BackBtn />
+    </>
+  );
+}

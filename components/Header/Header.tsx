05da032de8d78dcc05b15ef57d0c3ee287760fa4
Onyshchenko Serhii <123294835+19taurus79@users.noreply.github.com>
2025-08-25 12:41:27 +0300

    refactor: uncomment initData state and useEffect for Telegram WebApp integration

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -32,15 +32,15 @@ function Header() {
     }
   };
   //TODO: get initData from Telegram WebApp
-  // const [initData, setInitData] = useState<string | null>(null);
+  const [initData, setInitData] = useState<string | null>(null);
 
-  // useEffect(() => {
-  //   if (typeof window !== "undefined" && window.Telegram?.WebApp?.initData) {
-  //     setInitData(window.Telegram.WebApp.initData);
-  //   }
-  // }, []);
-  const initData =
-    "user=%7B%22id%22%3A548019148%2C%22first_name%22%3A%22%D0%A1%D0%B5%D1%80%D0%B3%D0%B5%D0%B9%22%2C%22last_name%22%3A%22%D0%9E%D0%BD%D0%B8%D1%89%D0%B5%D0%BD%D0%BA%D0%BE%22%2C%22username%22%3A%22OnyshchenkoSergey%22%2C%22language_code%22%3A%22uk%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2Fqf0qiya3lYZumE5ExiC55ONcmy-5vzP6pZzzBMV92vw.svg%22%7D&chat_instance=6192166653529292056&chat_type=sender&auth_date=1755863330&signature=xK_kiByWRV0odX2jAJ0muhtoJEr64GRvSfREKixBOc7SCaDpzcrV53L_RiC-IZjhrMScWvGHGs7on6AD1uBbDQ&hash=d8067628db8f1ac8909c15fbe3ab4d876db16ff8177bb1ad5c1ddc51e2e01bb0";
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.Telegram?.WebApp?.initData) {
+      setInitData(window.Telegram.WebApp.initData);
+    }
+  }, []);
+  // const initData =
+  //   "user=%7B%22id%22%3A548019148%2C%22first_name%22%3A%22%D0%A1%D0%B5%D1%80%D0%B3%D0%B5%D0%B9%22%2C%22last_name%22%3A%22%D0%9E%D0%BD%D0%B8%D1%89%D0%B5%D0%BD%D0%BA%D0%BE%22%2C%22username%22%3A%22OnyshchenkoSergey%22%2C%22language_code%22%3A%22uk%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2Fqf0qiya3lYZumE5ExiC55ONcmy-5vzP6pZzzBMV92vw.svg%22%7D&chat_instance=6192166653529292056&chat_type=sender&auth_date=1755863330&signature=xK_kiByWRV0odX2jAJ0muhtoJEr64GRvSfREKixBOc7SCaDpzcrV53L_RiC-IZjhrMScWvGHGs7on6AD1uBbDQ&hash=d8067628db8f1ac8909c15fbe3ab4d876db16ff8177bb1ad5c1ddc51e2e01bb0";
   useEffect(() => {
     const fetchUser = async () => {
       if (initData) {
@@ -49,7 +49,7 @@ function Header() {
       }
     };
     fetchUser();
-  }, []);
+  }, [initData]);
   console.log("initData", initData);
   console.log("userData", userData);
   const handleNavClick = () => {

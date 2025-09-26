5d3784e96b1a74dd93f712bd2a758cf41690fdac
Onyshchenko Serhii <123294835+19taurus79@users.noreply.github.com>
2025-09-26 13:25:38 +0300

    feat: refactor DetailTaskPage to use TaskClientPage component and remove unused imports

---
+++ app/tasks/[task]/TaskClientPage.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import BackBtn from "@/components/BackBtn/BackBtn";
+import TaskCart from "@/components/TaskCart/TaskCart";
+import TasksBtn from "@/components/TasksBtn/TasksBtn";
+import { useInitData } from "@/store/InitData";
+import { useUser } from "@/store/User";
+import { TaskGoogle, TaskStatus } from "@/types/types";
+
+type Props = {
+  task: TaskGoogle;
+  taskStatus: TaskStatus;
+  fromLink: boolean;
+};
+
+export default function TaskClientPage({ task, taskStatus, fromLink }: Props) {
+  const userData = useUser((state) => state.userData);
+  const initData = useInitData((state) => state.initData);
+
+  console.log("User from store:", userData);
+  console.log("InitData from store:", initData);
+
+  return (
+    <>
+      <TaskCart task={task} taskStatus={taskStatus} />
+      {userData?.is_admin && (
+        <TasksBtn taskId={task.id} taskStatus={taskStatus} />
+      )}
+      <BackBtn isClose={fromLink} />
+    </>
+  );
+}

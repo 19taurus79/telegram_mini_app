25bf11d5d62fecff93c44b74a0638679897328af
Onyshchenko Serhii <123294835+19taurus79@users.noreply.github.com>
2025-11-26 17:52:25 +0200

    feat: Добавлен модуль карты с адаптивным дизайном, компонентами для отображения данных, ввода адреса и управления слоями, а также новые стили и изображения.

--- components/MapModule/leaflet-icon.js
+++ components/MapModule/leaflet-icon.js
@@ -9,3 +9,13 @@ export const customIcon = new L.Icon({
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
 });
+
+export const clientIcon = new L.Icon({
+  iconUrl: "/images/client.png",
+  // iconRetinaUrl: "/images/marker-icon-2x.png",
+  // shadowUrl: "/images/marker-shadow.png",
+  iconSize: [24, 24],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  // shadowSize: [41, 41],
+});
--- components/MapModule/store/mapControlStore.js
+++ components/MapModule/store/mapControlStore.js
@@ -3,10 +3,14 @@ import { create } from "zustand";
 export const useMapControlStore = create((set) => ({
   areApplicationsVisible: false,
   showHeatmap: false,
+  areClientsVisible: false,
   
   setApplicationsVisible: (visible) => set({ areApplicationsVisible: visible }),
   toggleApplications: () => set((state) => ({ areApplicationsVisible: !state.areApplicationsVisible })),
   
   setShowHeatmap: (show) => set({ showHeatmap: show }),
   toggleHeatmap: () => set((state) => ({ showHeatmap: !state.showHeatmap })),
+
+  setClientsVisible: (visible) => set({ areClientsVisible: visible }),
+  toggleClients: () => set((state) => ({ areClientsVisible: !state.areClientsVisible })),
 }));

58c16b76676eb2769493af3d142960838c448d06
Onyshchenko Serhii <123294835+19taurus79@users.noreply.github.com>
2026-09-14 14:02:42 +0300

    feat: add AvStockPage server component to fetch and display product remains by ID

--- app/av_stock/[id]/page.tsx
+++ app/av_stock/[id]/page.tsx
@@ -12,12 +12,16 @@ export default async function AvStockPage({ params }: Props) {
   const initData = await getInitData();
   const remains = await getAvRemainsById({ productId: id.id, initData });
   console.log("av stock remains", remains);
+  const title =
+    remains && remains.length > 0
+      ? `${remains[0].nomenclature} вільно по РУ`
+      : "Залишки товару відсутні";
   return (
     <>
       <div className={css.pageContent}>
         <div className={css.container}>
-          <h2 className={css.heading}>{remains[0].nomenclature} вільно по РУ</h2>
-          <AvRemainsListClient remains={remains} />
+          <h2 className={css.heading}>{title}</h2>
+          <AvRemainsListClient remains={remains || []} />
         </div>
       </div>
       {/* <BackBtn /> */}
